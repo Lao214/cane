b7322d232b0fabeec8851a349ef491b40a7b65e2
echoes <1085444086@qq.com>
2025-04-15 22:16:01 +0800

    变筛选

--- src/views/resour/fusui.vue
+++ src/views/resour/fusui.vue
@@ -1,21 +1,58 @@
 <template>
     <div class="body">
+
+        <!-- 搜索框 -->
+        <!-- <div class="input-container">
+            <input v-model="searchObj.keyword" placeholder="用搜索更简单" type="text" class="input">
+            <span @click="fetchData()">立刻搜索</span>
+        </div> -->
+
+        <div style="display: flex;gap: 2rem;">
+            <!-- 搜索框 -->
+            <el-select v-model="searchObj.yangPlant" placeholder="请选择样地" @change="fetchData()">
+                <el-option   v-for="item in options" :key="item.value"  :label="item.label"  :value="item.value">
+                </el-option>
+            </el-select>
+
+            <el-select v-model="searchObj.yangFang" placeholder="请选择样方" @change="fetchData()">
+                <el-option   v-for="item in options2" :key="item.value"  :label="item.label"  :value="item.value">
+                </el-option>
+            </el-select>
+        </div>
+
+
         <h1>扶绥基地数据</h1>
 
-        <div class="archive">
-            <div class="archive-content">
-                <p class="archive-title">归档</p>
-                <div class="archive-list-tag"><i class="el-icon-date"></i> 列表</div>
-                <div class="archive-list">
-                    <div v-for="(item, index) in cardList" :key="index" class="archive-list-item" @click="viewResour(item)">
-                        <span>{{ item.baseName }}</span>
-                        <span>{{ item.measurementDate }}</span>
-                    </div>
-                </div>
-                <div class="archive-list-footer">
-                    <el-pagination background layout="prev, pager, next" :total="total" @current-change="fetchData" style="margin-bottom: 34px;"></el-pagination>
-                </div>
-            </div>
+        <div class="table-box">
+            <el-table :data="cardList" style="width: 100%">
+                <el-table-column prop="baseName" label="基地名称" width="180"></el-table-column>
+                <el-table-column prop="varietyName" label="品种名" width="180"></el-table-column>
+                <el-table-column prop="measurementDate" label="测定日期" width="180"></el-table-column>
+                <el-table-column prop="yangPlant" label="样地" width="180"></el-table-column>
+                <el-table-column prop="yangFang" label="样方" width="180"></el-table-column>
+                <el-table-column prop="zhuGaoNum" label="株数编号" width="180"></el-table-column>
+                <el-table-column prop="zhuGao" label="株高" width="180"></el-table-column>
+                <el-table-column prop="zhuGaoAve10" label="平均株高（10株）" width="180"></el-table-column>
+                <el-table-column prop="stemDiameter" label="茎径" width="180"></el-table-column>
+                <el-table-column prop="stemDiameterAve10" label="平均茎径（10株）" width="180"></el-table-column>
+                <el-table-column prop="chui" label="锤度" width="180"></el-table-column>
+                <el-table-column prop="lai" label="LAI" width="180"></el-table-column>
+                <el-table-column prop="laiAve" label="LAI平均" width="180"></el-table-column>
+                <el-table-column prop="mta" label="MTA" width="180"></el-table-column>
+                <el-table-column prop="mtaAve" label="MTA平均" width="180"></el-table-column>
+                <el-table-column prop="ylshl" label="叶绿素含量" width="180"></el-table-column>
+                <el-table-column prop="ylshlAve" label="平均叶绿素含量" width="180"></el-table-column>
+                <el-table-column prop="xianzhong3" label="鲜重（三株）" width="180"></el-table-column>
+                <el-table-column prop="xianzhongAve" label="平均鲜重" width="180"></el-table-column>
+                <el-table-column prop="ganzhong3" label="干重（三株）" width="180"></el-table-column>
+                <el-table-column prop="ganzhongAve" label="平均干重" width="180"></el-table-column>
+                <el-table-column prop="miaoshu" label="苗数" width="180"></el-table-column>
+                <el-table-column prop="miaoshuAve" label="平均苗数（第2、3、4行）" width="180"></el-table-column>
+
+            </el-table>
+
+            <!-- 分页组件 -->
+            <el-pagination :current-page="page" :total="total" :page-size="limit"  style="padding: 30px 0; text-align: center;" layout="total, prev, pager, next, jumper" @current-change="fetchData" />
         </div>
 
         
@@ -38,24 +75,39 @@ export default {
             limit: 10, // 每页记录数
             cardList: [],
             searchObj: {
-                keyword: '',
-                category: 'all',
-                sortType: 'times',
-                resistanceLevelHan: '',
-                resistanceLevelHei: '',
-                resistanceLevel: ''
+                yangPlant: '',
+                yangFang: '',
             },
-            hanSelected: '',
-            hanOptions: [],
-            coldSelected: '',
-            coldOptions: [],
-            smutSelected: '',
-            smutOptions: [],
+            options: [
+                { value: '', label: '全部' },
+                { value: 'A1', label: 'A1' },
+                { value: 'A2', label: 'A2' },
+                { value: 'A3', label: 'A3' },
+                { value: 'A4', label: 'A4' },
+                { value: 'A5', label: 'A5' },
+                { value: 'B1', label: 'B1' },
+                { value: 'B2', label: 'B2' },
+                { value: 'B3', label: 'B3' },
+                { value: 'B4', label: 'B4' },
+                { value: 'B5', label: 'B5' },
+                { value: 'C1', label: 'C1' },
+                { value: 'C2', label: 'C2' },
+                { value: 'C3', label: 'C3' },
+                { value: 'C4', label: 'C4' },
+                { value: 'C5', label: 'C5' }
+            ],
+            options2: [
+                { value: '', label: '全部' },
+                { value: '1', label: '1' },
+                { value: '2', label: '2' },
+                { value: '3', label: '3' },
+                { value: '4', label: '4' },
+                { value: '5', label: '5' },
+                { value: '6', label: '6' },
+            ],
         }
     },
     async created() {
-        // 设置 searchObj.keyword 为查询参数 'skword'
-        this.searchObj.keyword = this.$route.query.skword
 
         await this.fetchData()
 
@@ -157,5 +209,91 @@ export default {
 .archive-list-footer {
     text-align: center;
     margin-top: 3rem;
+}
+
+.input-container {
+  position: relative;
+  display: flex;
+  margin: 0 auto;
+  width: 100%;
+  min-width: 490px;
+  width: 47%;
+}
+
+.input-container > span,
+.input-container .input {
+  white-space: nowrap;
+  display: block;
+}
+
+.input-container > span,
+.input-container .input:first-child {
+  /* border-radius: 6px 0 0 6px; */
+}
+
+.input-container > span,
+.input-container .input {
+  /* border-radius: 0 6px 6px 0; */
+}
+
+.input-container > span,
+.input-container .input {
+  margin-left: -1px;
+}
+
+.input-container .input {
+  position: relative;
+  z-index: 1;
+  flex: 1 1 auto;
+  width: 1%;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.input-container span {
+  text-align: center;
+  padding: 8px 20px;
+  font-size: 16px;
+  line-height: 25px;
+  color: #fff;
+  background: #32b753;
+  border: 1px solid #CDD9ED;
+  font-weight: bold;
+  transition: background 0.3s ease, border 0.3s ease, color 0.3s ease;
+}
+
+.input-container:focus-within > span {
+  color: #fff;
+  /* background-color: #148cd1; */
+  /* border-color: #148cd1; */
+  background-color: #39bda7;
+  border-color: #39bda7;
+  cursor: pointer;
+}
+
+.input {
+  display: block;
+  width: 100%;
+  padding: 8px 16px;
+  line-height: 25px;
+  font-size: 14px;
+  font-weight: 500;
+  font-family: inherit;
+  /* border-radius: 6px; */
+  -webkit-appearance: none;
+  color: #99A3BA;
+  border: 1px solid  #CDD9ED;
+  background: #fff;
+  transition: border 0.3s ease;
+}
+
+.input::placeholder {
+  color: #CBD1DC;
+}
+
+.input:focus {
+  outline: none;
+  /* border-color: #148cd1; */
+  border-color:#39bda7 ;
 }
 </style>
--- src/views/resour/wuming.vue
+++ src/views/resour/wuming.vue
@@ -1,21 +1,101 @@
 <template>
     <div class="body">
+
+        <div style="display: flex;gap: 2rem;">
+            <!-- 搜索框 -->
+            <el-select v-model="searchObj.samplePlot" placeholder="请选择样地" @change="shaixuan">
+                <el-option   v-for="item in options" :key="item.value"  :label="item.label"  :value="item.value">
+                </el-option>
+            </el-select>
+
+            <el-select v-model="searchObj.sampleFang" placeholder="请选择样方" @change="shaixuan">
+                <el-option   v-for="item in options2" :key="item.value"  :label="item.label"  :value="item.value">
+                </el-option>
+            </el-select>
+        </div>
+
         <h1>扶绥基地数据</h1>
 
-        <div class="archive">
-            <div class="archive-content">
-                <p class="archive-title">归档</p>
-                <div class="archive-list-tag"><i class="el-icon-date"></i> 列表</div>
-                <div class="archive-list">
-                    <div v-for="(item, index) in cardList" :key="index" class="archive-list-item" @click="viewResour(item)">
-                        <span>{{ item.baseName }}</span>
-                        <span>{{ item.measurementDate }}</span>
-                    </div>
-                </div>
-                <div class="archive-list-footer">
-                    <el-pagination background layout="prev, pager, next" :total="total" @current-change="fetchData" style="margin-bottom: 34px;"></el-pagination>
-                </div>
-            </div>
+        <div class="table-box">
+            <el-table :data="cardList" style="width: 100%">
+                <el-table-column prop="baseName" label="基地名称" width="180"></el-table-column>
+                <el-table-column prop="varietyName" label="品种名" width="180"></el-table-column>
+                <el-table-column prop="measurementDate" label="测定日期" width="180"></el-table-column>
+                <el-table-column prop="samplePlot" label="样地" width="180"></el-table-column>
+                <el-table-column prop="sampleFang" label="样方" width="180"></el-table-column>
+                <el-table-column prop="waterContentXian10cm" label="土壤含水量% 鲜重 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="waterContentGan10cm" label="土壤含水量% 干重 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="waterContentFen10cm" label="土壤含水量分析基% 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="waterContentFenAverage10cm" label="土壤平均含水量分析基% 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="waterContentGanji10cm" label="土壤含水量干基% 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="waterContentGanjiAverage10cm" label="土壤平均含水量干基% 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="waterContentXian20cm" label="土壤含水量% 鲜重 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="waterContentGan20cm" label="土壤含水量% 干重 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="waterContentFen20cm" label="土壤含水量分析基% 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="waterContentFenAverage20cm" label="土壤平均含水量分析基% 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="waterContentGanji20cm" label="土壤含水量干基% 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="waterContent10cm" label="土壤含水量 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="waterContent20cm" label="土壤含水量 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="gFeso410cm" label="FeSO4取样重量 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="mlFeso410cm" label="FeSO4滴定体积 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="gkgFeso410cm" label="FeSO4有机质含量 g/kg 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="gFeso420cm" label="FeSO4取样重量 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="mlFeso420cm" label="FeSO4滴定体积 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="gkgFeso420cm" label="FeSO4有机质含量 g/kg 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="gK10cm" label="钾取样重量 g 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="mglK10cm" label="钾浓度 mg/l 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="mgkgK10cm" label="钾含量 mg/kg 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="gK20cm" label="钾取样重量 g 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="mglK20cm" label="钾浓度 mg/l 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="mgkgK20cm" label="钾含量 mg/kg 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="gN210cm" label="全氮取样重量 g 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="mlv1N210cm" label="全氮滴定体积V1 ml 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="mlv2N210cm" label="全氮滴定体积V2 ml 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="mlv0N210cm" label="全氮滴定体积V0 ml 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="gkgN210cm" label="全氮含量 g/kg 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="gN220cm" label="全氮取样重量 g 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="mlv1N220cm" label="全氮滴定体积V1 ml 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="mlv2N220cm" label="全氮滴定体积V2 ml 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="mlv0N220cm" label="全氮滴定体积V0 ml 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="gkgN220cm" label="全氮含量 g/kg 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="gP10cm" label="磷取样重量 g 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="airP10cm" label="磷空白吸光度 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="yangP10cm" label="磷样品吸光度 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="ugmlP10cm" label="磷浓度 μg/ml 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="mgkgP10cm" label="磷含量 mg/kg 0-10cm" width="180"></el-table-column>
+                <el-table-column prop="gP20cm" label="磷取样重量 g 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="airP20cm" label="磷空白吸光度 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="yangP20cm" label="磷样品吸光度 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="ugmlP20cm" label="磷浓度 μg/ml 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="mgkgP20cm" label="磷含量 mg/kg 10-20cm" width="180"></el-table-column>
+                <el-table-column prop="averagePlantHeight" label="平均株高（m）" width="180"></el-table-column>
+                <el-table-column prop="averageStemDiameter" label="平均茎径（mm）" width="180"></el-table-column>
+                <el-table-column prop="totalStemWeight" label="总茎重（KG）" width="180"></el-table-column>
+                <el-table-column prop="numberEffectiveStems" label="有效茎数" width="180"></el-table-column>
+                <el-table-column prop="stemFreshWeight" label="茎鲜重（g/plant）" width="180"></el-table-column>
+                <el-table-column prop="stemFreshWeightAve" label="平均茎鲜重（g/plant）" width="180"></el-table-column>
+                <el-table-column prop="gPlant" label="绿叶片鲜重(g/plant)" width="180"></el-table-column>
+                <el-table-column prop="gPlantAve" label="平均绿叶片鲜重(g/plant)" width="180"></el-table-column>
+                <el-table-column prop="gPlantShao" label="绿叶鞘鲜重(g/plant)" width="180"></el-table-column>
+                <el-table-column prop="gPlantShaoAve" label="平均绿叶鞘鲜重(g/plant)" width="180"></el-table-column>
+                <el-table-column prop="gPlantGanZhong" label="茎干重(g/plant)" width="180"></el-table-column>
+                <el-table-column prop="gPlantGanZhongAve" label="平均茎干重(g/plant)" width="180"></el-table-column>
+                <el-table-column prop="jingWaterContent" label="茎含水量(%)" width="180"></el-table-column>
+                <el-table-column prop="jingWaterContentAve" label="平均茎含水量(%)" width="180"></el-table-column>
+                <el-table-column prop="yeWaterContent" label="绿叶片含水量(%)" width="180"></el-table-column>
+                <el-table-column prop="yeWaterContentAve" label="绿叶叶鞘含水量(%)" width="180"></el-table-column>
+                <el-table-column prop="bx" label="平均田间钻汁锤度(゜Bx)" width="180"></el-table-column>
+                <el-table-column prop="yxc" label="叶序长（cm）" width="180"></el-table-column>
+                <el-table-column prop="yxk" label="叶序宽（cm）" width="180"></el-table-column>
+                <el-table-column prop="ymj" label="叶面积(cm2/leaf)" width="180"></el-table-column>
+                <el-table-column prop="ymjAve" label="平均叶面积(cm2/leaf)" width="180"></el-table-column>
+                <el-table-column prop="qys" label="青叶数" width="180"></el-table-column>
+                <el-table-column prop="qysAve" label="平均青叶数" width="180"></el-table-column>
+
+            </el-table>
+
+            <!-- 分页组件 -->
+            <el-pagination :current-page="page" :total="total" :page-size="limit"  style="padding: 30px 0; text-align: center;" layout="total, prev, pager, next, jumper" @current-change="fetchData" />
         </div>
 
         
@@ -38,24 +118,43 @@ export default {
             limit: 10, // 每页记录数
             cardList: [],
             searchObj: {
-                keyword: '',
-                category: 'all',
-                sortType: 'times',
-                resistanceLevelHan: '',
-                resistanceLevelHei: '',
-                resistanceLevel: ''
+                samplePlot: '',
+                sampleFang: '',
             },
-            hanSelected: '',
-            hanOptions: [],
-            coldSelected: '',
-            coldOptions: [],
-            smutSelected: '',
-            smutOptions: [],
+            options: [
+                { value: '', label: '全部' },
+                { value: '1--1', label: '1--1' },
+                { value: '1--2', label: '1--2' },
+                { value: '1--3', label: '1--3' },
+                { value: '2--1', label: '2--1' },
+                { value: '2--2', label: '2--2' },
+                { value: '2--3', label: '2--3' },
+                { value: '3--1', label: '3--1' },
+                { value: '3--2', label: '3--2' },
+                { value: '3--3', label: '3--3' },
+                { value: '4--1', label: '4--1' },
+                { value: '4--2', label: '4--2' },
+                { value: '4--3', label: '4--3' },
+                { value: '5--1', label: '5--1' },
+                { value: '5--2', label: '5--2' },
+                { value: '5--3', label: '5--3' },
+                { value: '6--1', label: '6--1' },
+                { value: '6--2', label: '6--2' },
+                { value: '6--3', label: '6--3' }
+            ],
+            options2: [
+                { value: '', label: '全部' },
+                { value: '1', label: '1' },
+                { value: '2', label: '2' },
+                { value: '3', label: '3' },
+                { value: '4', label: '4' },
+                { value: '5', label: '5' },
+                { value: '6', label: '6' },
+            ],
+
         }
     },
     async created() {
-        // 设置 searchObj.keyword 为查询参数 'skword'
-        this.searchObj.keyword = this.$route.query.skword
 
         await this.fetchData()
 
@@ -65,7 +164,13 @@ export default {
             console.log(item)
             window.open(`#/wumingDetails?tolname=${item.baseName}&caneId=${item.id}`)
         },
+        async shaixuan(val) {
+            console.log(this.searchObj,'this.searchObj')
+            console.log(val,'val')
+            await this.fetchData()
+        },
         fetchData(page = 1) {
+            console.log(this.searchObj)
             this.page = page
             wumingApi.getPageData(this.page, this.limit, this.searchObj).then(response => {
                 if(response.code === 200) {
@@ -157,5 +262,66 @@ export default {
 .archive-list-footer {
     text-align: center;
     margin-top: 3rem;
+}
+
+
+.input-container {
+  position: relative;
+  display: flex;
+  margin: 0 auto;
+  width: 100%;
+  min-width: 490px;
+  width: 47%;
+}
+
+.input-container > span,
+.input-container .input {
+  white-space: nowrap;
+  display: block;
+}
+
+.input-container > span,
+.input-container .input:first-child {
+  /* border-radius: 6px 0 0 6px; */
+}
+
+.input-container > span,
+.input-container .input {
+  /* border-radius: 0 6px 6px 0; */
+}
+
+.input-container > span,
+.input-container .input {
+  margin-left: -1px;
+}
+
+.input-container .input {
+  position: relative;
+  z-index: 1;
+  flex: 1 1 auto;
+  width: 1%;
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.input-container span {
+  text-align: center;
+  padding: 8px 20px;
+  font-size: 16px;
+  line-height: 25px;
+  color: #fff;
+  background: #32b753;
+  border: 1px solid #CDD9ED;
+  font-weight: bold;
+  transition: background 0.3s ease, border 0.3s ease, color 0.3s ease;
+}
+
+.input-container:focus-within > span {
+  color: #fff;
+  /* background-color: #148cd1; */
+  /* border-color: #148cd1; */
+  background-color: #39bda7;
+  border-color: #39bda7;
+  cursor: pointer;
 }
 </style>
